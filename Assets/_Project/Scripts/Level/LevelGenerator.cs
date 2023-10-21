ceaf3513d86e7f6bfafd66b8148375681a207f31
busraasevimm <busraasevimm@gmail.com>
2023-10-21 06:26:27 +0300

    Level generator (Part IV: active islands)

--- Assets/_Project/Scripts/Level/LevelGenerator.cs
+++ Assets/_Project/Scripts/Level/LevelGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Zenject;
 
@@ -10,14 +11,15 @@ public class LevelGenerator
     private Vector3[] _islandPositions;
     private StickManager _stickManager;
     private List<Island> _islands;
-    private Island[] _levelIslands;
+    private List<Island> _levelIslands;
     private GameSettings _settings;
     private MatchController _matchController;
 
     private int _currentLevelIslandCount;
     private int _currentLevelColorCount;
 
-    public LevelGenerator(LevelManager levelManager, ObjectPool pool, StickManager stickManager, GameSettings settings,MatchController controller)
+    public LevelGenerator(LevelManager levelManager, ObjectPool pool, StickManager stickManager, GameSettings settings,
+        MatchController controller)
     {
         _levelManager = levelManager;
         _objectPool = pool;
@@ -25,32 +27,33 @@ public class LevelGenerator
         _settings = settings;
         _matchController = controller;
 
-        _islandPositions = new Vector3[10];
+        _islandPositions = new Vector3[12];
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 6; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < 2; j++)
             {
-                var position = new Vector3(i * 4, 0f, j * 2);
-                _islandPositions[i * 5 + j] = position;
+                var position = new Vector3(j * 4, 0f, i * 1.2f);
+                _islandPositions[i * 2 + j] = position;
             }
         }
 
         _islands = new List<Island>();
+        GenerateIslands();
     }
 
     public void GenerateLevel()
     {
         SetLevelIslandAndColorCount(_levelManager.CurrentLevelNo);
-        GenerateIslands();
+        GenerateLevelIslands();
         _stickManager.GenerateSticks(_currentLevelColorCount);
-        
+
         var sticks = _stickManager.GetLevelSticks();
         var islandIndex = 0;
         for (int i = 0; i < sticks.Count; i++)
         {
             Island.SlotGroup group = null;
-            
+
             if (_levelIslands[islandIndex].TryGetEmptySlotGroup(out group))
             {
                 sticks[i].ChangeGroupPosition(group);
@@ -64,7 +67,7 @@ public class LevelGenerator
                 }
             }
         }
-        
+
         Debug.Log("Level is generated.");
     }
 
@@ -72,7 +75,7 @@ public class LevelGenerator
     {
         for (int i = 0; i < _islandPositions.Length; i++)
         {
-            var rotation = Quaternion.Euler(0f, i >= _islandPositions.Length / 2 ? -90f : 90f, 0f);
+            var rotation = Quaternion.Euler(0f, i % 2 == 0 ? 90f : -90f, 0f);
             var island = _objectPool.SpawnFromPool(PoolTags.Island, _islandPositions[i], rotation)
                 .GetComponent<Island>();
 
@@ -80,11 +83,21 @@ public class LevelGenerator
             island.Deactivate();
             _islands.Add(island);
         }
+    }
 
-        _levelIslands = _islands.GetRandomElements(_currentLevelIslandCount);
-        foreach (var island in _levelIslands)
+    private void GenerateLevelIslands()
+    {
+        var allIslands = new List<Island>();
+        allIslands.AddRange(_islands);
+        _levelIslands = new List<Island>();
+        
+        for (int i = 0; i < _currentLevelIslandCount; i++)
         {
-            island.Activate();
+            var count = allIslands.Count;
+            var index = Mathf.RoundToInt(count / 2);
+            allIslands[index].Activate();
+            _levelIslands.Add(allIslands[index]);
+            allIslands.RemoveAt(index);
         }
     }
 
@@ -94,24 +107,28 @@ public class LevelGenerator
         {
             _currentLevelColorCount = 2;
             _currentLevelIslandCount = 3;
-        }else if (currentLevelNumber < 6)
+        }
+        else if (currentLevelNumber < 6)
         {
             _currentLevelColorCount = 3;
             _currentLevelIslandCount = 5;
-        }else if (currentLevelNumber < 10)
+        }
+        else if (currentLevelNumber < 10)
         {
             _currentLevelColorCount = 4;
             _currentLevelIslandCount = 6;
-        }else if (currentLevelNumber < 17)
+        }
+        else if (currentLevelNumber < 17)
         {
             _currentLevelColorCount = 5;
             _currentLevelIslandCount = 8;
-        }else if (currentLevelNumber < 22)
+        }
+        else if (currentLevelNumber < 22)
         {
             _currentLevelColorCount = 5;
             _currentLevelIslandCount = 7;
         }
-        else if(currentLevelNumber<32)
+        else if (currentLevelNumber < 32)
         {
             _currentLevelColorCount = 6;
             _currentLevelIslandCount = 9;
