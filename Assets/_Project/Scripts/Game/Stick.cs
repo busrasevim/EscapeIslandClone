b3f07c6de6166c40110b1f2f8e59ec2e2ebda751
busraasevimm <busraasevimm@gmail.com>
2023-10-21 07:29:32 +0300

    Stick model and animations added.

--- Assets/_Project/Scripts/Game/Stick.cs
+++ Assets/_Project/Scripts/Game/Stick.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Stick : MonoBehaviour
 {
     private Island.SlotGroup _currentSlotGroup;
+    [SerializeField] private Animator animator;
+    private const string RUN_ANIMATION_BOOL_KEY = "Run";
 
     public void PrepareStick(Color color)
     {
@@ -15,7 +17,12 @@ public class Stick : MonoBehaviour
     public void GoNewPlace(Vector3 position, Island.SlotGroup group)
     {
         _currentSlotGroup = group;
-        transform.DOLocalMove(position, 1f);
+        animator.SetBool(RUN_ANIMATION_BOOL_KEY, true);
+        transform.DOLocalMove(position, 1f).OnComplete(() =>
+        {
+            animator.SetBool(RUN_ANIMATION_BOOL_KEY, false);
+            transform.DORotate(group.currentIsland.transform.eulerAngles, 0.5f);
+        });
     }
 
     public void Deactivate()
